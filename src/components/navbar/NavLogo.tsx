c2977f07c6bd614f5156e5c6a114f7cd64d9638e
ricmel111 <40599467+ricmel111@users.noreply.github.com>
2025-07-23 01:11:18 +0100

    Logo update

--- src/components/navbar/NavLogo.tsx
+++ src/components/navbar/NavLogo.tsx
@@ -7,17 +7,21 @@ interface NavLogoProps {
 }
 
 const NavLogo = ({ isHomePage, isScrolled }: NavLogoProps) => {
+  const logoSrc =
+    isHomePage && !isScrolled
+      ? '/BibleQuestLogo.png' // default full homepage logo
+      : '/BibleQuestLogoDark.png'; // smaller or alternative logo for all other cases
+
   return (
     <div className={`flex items-center ${!isScrolled ? 'pt-5' : ''}`}>
       <Link to="/" className="flex items-center">
-        <img 
-          src="/BibleQuestLogo.png"
+        <img
+          src={logoSrc}
           alt="Bible Quest Logo"
           className={`transition-all duration-300 ${
             isHomePage && !isScrolled ? 'h-16 w-auto' : 'h-10 w-auto'
-          }`} 
+          }`}
         />
-
       </Link>
     </div>
   );
